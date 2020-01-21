7af877a3f6cfafb7cdb9b4cf9d835afe275abc36
4KColo <xy37@duke.edu>
2020-01-21 11:33:15 -0500

    point source

---
+++ linearized_hydro_gauss.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import numpy as np
+import h5py
+import os
+import multiprocessing as mp
+import ctypes
+from time import time
+from evolution import Tau_to_Temp
+from evolution import RK4
+from evolution import Gauss_Source
+from evolution import Point_Source
+from constants import *
+
+e_jet = 100.0               # initial jet energy in GeV
+x0 = 0.0/C1                 # initial position of the jet
+y0 = 0.0/C1
+eta0 = 0.0
+
+para_gauss_width = 1.       # sigma = #/(pi*T), width in Gauss energy deposition
+kappa_sc = 1.               # parameter in the Eloss formula
+viscosity_over_s = 0.1      # eta/s
+
+tau_stop = 6.0/C1           # time of jet scaping QGP
+dtau = 0.01/C1              # time step
+nstep = int((tau_f-tau_i)/dtau)+1
+nstep_stop = int((tau_stop-tau_i)/dtau)+1
+list_tau = np.linspace(tau_i, tau_f, nstep)
+
+list_x = x0 + list_tau      # positions of the jet at each time step
+list_y = y0 + 0.0*list_tau
+list_eta = eta0 + 0.0*list_tau
+list_temp = Tau_to_Temp(list_tau)
+
+
+def Linearized_Hydro(Kx, Ky, Keta):
+    re_e_new, im_e_new, re_gx_new, im_gx_new, re_gy_new, im_gy_new, re_geta_new, im_geta_new = 0., 0., 0., 0., 0., 0., 0., 0.
+    
+    for i in range(nstep):
+        # source for perturbations in each step
+        re_e_source, im_e_source = Gauss_Source(Kx, Ky, Keta, list_tau[i], dtau, list_x[i], list_y[i], list_eta[i], e_jet, list_temp[i], kappa_sc, para_gauss_width)
+        re_gx_source, im_gx_source = Gauss_Source(Kx, Ky, Keta, list_tau[i], dtau, list_x[i], list_y[i], list_eta[i], e_jet, list_temp[i], kappa_sc, para_gauss_width)
+        re_e_old  = re_e_new  + re_e_source
+        im_e_old  = im_e_new  + im_e_source
+        re_gx_old = re_gx_new + re_gx_source
+        im_gx_old = im_gx_new + im_gx_source
+        re_gy_old = re_gy_new
+        im_gy_old = im_gy_new
+        re_geta_old = re_geta_new
+        im_geta_old = im_geta_new
+                        
+        re_e_new, im_e_new, re_gx_new, im_gx_new, re_gy_new, im_gy_new, re_geta_new, im_geta_new = RK4(re_e_old, im_e_old, re_gx_old, im_gx_old, re_gy_old, im_gy_old, re_geta_old, im_geta_old, Kx, Ky, Keta, list_tau[i], dtau, cs_sqd, viscosity_over_s)
+        
+    return np.array([re_e_new, im_e_new, re_gx_new, im_gx_new, re_gy_new, im_gy_new, re_geta_new, im_geta_new])
+    
+
+### parallel computing
+N = 5
+kx = np.linspace(-5.0, 5.0, N)
+ky = np.linspace(-5.0, 5.0, N)
+keta = np.linspace(-5.0, 5.0, N)
+
+shared_array_base = mp.Array(ctypes.c_double, N*N*N*8)
+shared_array = np.ctypeslib.as_array(shared_array_base.get_obj())
+shared_array = shared_array.reshape(N,N,N,8)
+
+def Parallel_LinearHydro(X, Output = shared_array):
+    i = X[0]
+    j = X[1]
+    k = X[2]
+    Output[i][j][k] = Linearized_Hydro(kx[i], ky[j], keta[k])
+    return None
+    
+
+ti = time()
+
+Nprocess = mp.cpu_count()
+pool = mp.Pool(Nprocess)
+pool.map(Parallel_LinearHydro, [(i,j,k) for i in range(N) for j in range(N) for k in range(N)])
+
+tf = time()
+print tf - ti
+
+
+### save file
+filename = 'Linearized_Hydro_over_Bjorken.hdf5'
+if not os.path.exists(filename):
+    f = h5py.File(filename, 'w')
+else:
+    f = h5py.File(filename, 'a')
+    
+groupname = 'eta_over_s='+str(viscosity_over_s)
+if groupname in f:
+    del f[groupname]
+group = f.create_group(groupname)
+
+group.attrs.create('N', N)
+group.attrs.create('kx', kx)
+group.attrs.create('ky', ky)
+group.attrs.create('keta', keta)
+group.create_dataset('e_g', data = shared_array)
+
+f.close()
+
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# compare analytic and numerical solutions with eta/s = 0 and kappa = 0
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.special as ss
@@ -23,68 +25,82 @@ label_size = 16
 mpl.rcParams['xtick.labelsize'] = label_size 
 mpl.rcParams['ytick.labelsize'] = label_size 
 
-kx = 1.0    # GeV
-ky = 1.0
+kx = 10.0    # GeV
+ky = 10.0
 keta = 0.0
-x0 = 0.1    # GeV^-1
-y0 = 0.1
+x0 = 0.0    # GeV^-1
+y0 = 0.0
 eta0 = 0.0
-delta_E = 10.0
-viscosity_over_s = 0.0      # eta/s
+delta_E = 10.0/tau_i	# Delta E/tau, units: GeV^2
+viscosity_over_s_list = [0.0, 1./(4*np.pi), 2./(4*np.pi)]	# eta/s
 
-dtau = 0.01/C1              # time step
+a = 2.+cs_sqd				# for analytic solution
+b = cs_sqd*(kx*kx + ky*ky)
+
+dtau = 0.001/C1              # time step
 nstep = int((tau_f-tau_i)/dtau)+1
 list_tau = np.linspace(tau_i, tau_f, nstep)
 list_temp = Tau_to_Temp(list_tau)
 
-re_e = np.zeros(nstep)
-im_e = np.zeros(nstep)
-re_gx = np.zeros(nstep)
-im_gx = np.zeros(nstep)
-re_gy = np.zeros(nstep)
-im_gy = np.zeros(nstep)
-re_geta = np.zeros(nstep)
-im_geta = np.zeros(nstep)
+### analytic solution
+def A11(x_, a_, b_):
+	return x_**((1.-a_)/2.)*ss.jv((a_-1.)/2., np.sqrt(b_)*x_)
+	
+def A12(x_, a_, b_):
+	return x_**((1.-a_)/2.)*ss.yv((a_-1.)/2., np.sqrt(b_)*x_)
+	
+def A21(x_, a_, b_):
+	b_sqrt = np.sqrt(b_)
+	term1 = ((1.-a_)/2.+1.+cs_sqd)*x_**((1.-a_)/2.-1.)*ss.jv((a_-1.)/2., b_sqrt*x_)
+	term2 = b_sqrt/2. * x_**((1.-a_)/2.) * ( ss.jv((a_-1.)/2.-1., b_sqrt*x_) - ss.jv((a_-1.)/2.+1., b_sqrt*x_) )
+	return term1+term2
 
-re_e[0], im_e[0] = Point_Source_Test(kx, ky, keta, x0, y0, eta0, delta_E)
-re_gx[0], im_gx[0] = 0.0, 0.0
-re_gy[0], im_gy[0] = 0.0, 0.0
-re_geta[0], im_geta[0] = 0.0, 0.0
+def A22(x_, a_, b_):
+	b_sqrt = np.sqrt(b_)
+	term1 = ((1.-a_)/2.+1.+cs_sqd)*x_**((1.-a_)/2.-1.)*ss.yv((a_-1.)/2., b_sqrt*x_)
+	term2 = b_sqrt/2. * x_**((1.-a_)/2.) * ( ss.yv((a_-1.)/2.-1., b_sqrt*x_) - ss.yv((a_-1.)/2.+1., b_sqrt*x_) )
+	return term1+term2
 
-for i in range(nstep-1):
-    re_e[i+1], im_e[i+1], re_gx[i+1], im_gx[i+1], re_gy[i+1], im_gy[i+1], re_geta[i+1], im_geta[i+1] = RK4(re_e[i], im_e[i], re_gx[i], im_gx[i], re_gy[i], im_gy[i], re_geta[i], im_geta[i], kx, ky, keta, list_tau[i], dtau, cs_sqd, viscosity_over_s)
+matrix_A = np.array([[A11(tau_i, a, b), A12(tau_i, a, b)],[A21(tau_i, a, b), A22(tau_i, a, b)]])
+matrix_B = np.array([delta_E, 0.0])
+coeff_analytic = np.linalg.solve(matrix_A, matrix_B)
+sol_analytic = coeff_analytic[0]*A11(list_tau, a, b) + coeff_analytic[1]*A12(list_tau, a, b)
 
 
-### compare with analytic solution
-analytic_re = []
-analytic_im = []
-for i in range(nstep):
-    Tau = list_tau[i]
-    result = Tau**(-2./3)*(29.3328 - 5.94606*1j)*( ss.jv(2./3, 0.816497*Tau) + 0.949828*ss.yv(2./3, 0.816497*Tau) )
-    analytic_re.append(result.real)
-    analytic_im.append(result.imag)
-
-analytic_re = np.array(analytic_re)
-analytic_im = np.array(analytic_im)
-
+### comparison plots
 plt.figure()
-plt.plot(list_tau, re_e, linewidth = 2.0, color='black', label='numerical')
-plt.plot(list_tau, analytic_re, linewidth = 2.0, color='red', linestyle = '--', label='analytic')
-plt.xlabel(r'$\tau$(GeV$^{-1}$)', size = 20)
-plt.ylabel(r'$\Re\delta\tilde{\epsilon}$(GeV)', size = 20)
+plt.plot(list_tau*C1, sol_analytic, linewidth = 2.0, color='black', label='analytic, $4\pi\eta/s$=0')
+
+### numerical solutions
+for j in range(len(viscosity_over_s_list)):
+	viscosity_over_s = viscosity_over_s_list[j]
+	re_e = np.zeros(nstep)
+	im_e = np.zeros(nstep)
+	re_gx = np.zeros(nstep)
+	im_gx = np.zeros(nstep)
+	re_gy = np.zeros(nstep)
+	im_gy = np.zeros(nstep)
+	re_geta = np.zeros(nstep)
+	im_geta = np.zeros(nstep)
+
+	re_e[0], im_e[0] = Point_Source_Test(kx, ky, keta, x0, y0, eta0, delta_E)
+	re_gx[0], im_gx[0] = 0.0, 0.0
+	re_gy[0], im_gy[0] = 0.0, 0.0
+	re_geta[0], im_geta[0] = 0.0, 0.0
+	
+	for i in range(nstep-1):
+		re_e[i+1], im_e[i+1], re_gx[i+1], im_gx[i+1], re_gy[i+1], im_gy[i+1], re_geta[i+1], im_geta[i+1] = RK4(re_e[i], im_e[i], re_gx[i], im_gx[i], re_gy[i], im_gy[i], re_geta[i], im_geta[i], kx, ky, keta, list_tau[i], dtau, cs_sqd, viscosity_over_s)
+	if j == 0:
+		plt.plot(list_tau*C1, re_e, linewidth = 2.0, color='red', linestyle='--', label=r'numerical, $4\pi\eta/s$=0')
+	if j == 1:
+		plt.plot(list_tau*C1, re_e, linewidth = 2.0, color='blue', label=r'numerical, $4\pi\eta/s$='+str(j))
+	if j == 2:
+		plt.plot(list_tau*C1, re_e, linewidth = 2.0, color='green', label=r'numerical, $4\pi\eta/s$='+str(j))
+		
+plt.xlabel(r'$\tau$(fm/c)', size = 20)
+plt.ylabel(r'$\Re\delta\tilde{\epsilon}$(GeV$^2$)', size = 20)
 plt.legend(loc='best', fontsize = 16)
 plt.xticks(fontsize = 16)
 plt.yticks(fontsize = 16)
 plt.savefig('test_Re_e_kx='+str(kx)+'_ky='+str(ky)+'_x0='+str(x0)+'_y0='+str(y0)+'.pdf')
-plt.show()
-
-plt.figure()
-plt.plot(list_tau, im_e, linewidth = 2.0, color='black', label='numerical')
-plt.plot(list_tau, analytic_im, linewidth = 2.0, color='red', linestyle = '--', label='analytic')
-plt.xlabel(r'$\tau$(GeV$^{-1}$)', size = 20)
-plt.ylabel(r'$\Im\delta\tilde{\epsilon}$(GeV)', size = 20)
-plt.legend(loc='best', fontsize = 16)
-plt.xticks(fontsize = 16)
-plt.yticks(fontsize = 16)
-plt.savefig('test_Im_e_kx='+str(kx)+'_ky='+str(ky)+'_x0='+str(x0)+'_y0='+str(y0)+'.pdf')
 plt.show()
